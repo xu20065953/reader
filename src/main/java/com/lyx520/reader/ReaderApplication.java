82eaba9b0218c496d18ab11bfb21db580ae5322b
Jie-ZhouYi <20065953@163.com>
2019-08-24 20:50:15 +0800

    dsa

---
+++ src/main/java/com/lyx520/reader/ReaderApplication.java
@@ -0,0 +1,13 @@
+package com.lyx520.reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReaderApplication.class, args);
+    }
+
+}
